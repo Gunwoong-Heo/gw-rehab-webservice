14bb9d62e3f829346e1963c2d37b84fb344e3e4e
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-28 00:12:57 +0900

    Initial commit

---
+++ src/main/java/gw/gwrehabwebservice/GwRehabWebserviceApplication.java
@@ -0,0 +1,13 @@
+package gw.gwrehabwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwRehabWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwRehabWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gw/gwrehabwebservice/GwRehabWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package gw.gwrehabwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwRehabWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
