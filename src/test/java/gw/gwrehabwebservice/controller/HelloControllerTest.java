65391d4e7bb9437b5a7e6d1fccfa025c2623288a
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-28 21:37:38 +0900

    `helloDto 리턴 테스트` 추가

--- src/test/java/gw/gwrehabwebservice/controller/HelloControllerTest.java
+++ src/test/java/gw/gwrehabwebservice/controller/HelloControllerTest.java
@@ -1,15 +1,13 @@
-package gw.gwrehabwebservice.Controller;
+package gw.gwrehabwebservice.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 //- 간단하게 테스트하기 위해서는 @AutoConfigureMockMvc가 아닌 @WebMvcTest를 사용해야 한다.
@@ -29,13 +27,28 @@ class HelloControllerTest {
 
         // https://tech.devgd.com/12
         // https://howtodoinjava.com/spring-boot2/testing/spring-boot-mockmvc-example/
-        mockMvc.perform(MockMvcRequestBuilders
-                        .get("/hello")
+        mockMvc.perform(get("/hello")
 //                        .accept(MediaType.APPLICATION_JSON)
                 )
-//                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(hello))
+//                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello))
         ;
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        // given
+        String name = "Hello";
+        int amount = 1000;
+        // when
+        mockMvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+        // then
     }
 }
