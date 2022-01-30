872c21360ddee6f3a4dea6fc3ead5ad1cbc089f2
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-01-30 17:49:35 +0900

    `JpaConfig` 생성 후 `@EnableJpaAuditing` 이동 (HelloControllerTest 를 위해)

--- src/main/java/gw/gwrehabwebservice/GwRehabWebserviceApplication.java
+++ src/main/java/gw/gwrehabwebservice/GwRehabWebserviceApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+// HelloControllerTest 를 위해 주석처리(`@EnableJpaAuditing`이 `@SpringBootApplication`과 함께 있다보니, `@WebMvcTest`에서도 스캔)
+// `JpaConfig` 생성 후 `JpaConfig`에 `@EnableJpaAuditing`추가   
+//@EnableJpaAuditing
 @SpringBootApplication
 public class GwRehabWebserviceApplication {
 
---
+++ src/main/java/gw/gwrehabwebservice/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package gw.gwrehabwebservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
